f2d640e82fefca565f6ff9e6901716c9f6da5741
Otavio <otavioaugustobr@gmail.com>
2024-05-27 20:37:17 -0300

    Exclusão de arquivos inuteis

--- Controles.py
+++
@@ -1,7 +0,0 @@
-from Itens import Item
-
-controleIdItem = 0
-
-def aumentarIdItem():
-  controleIdItem = controleIdItem + 1
-
--- fornecedor.py
+++
@@ -1,6 +0,0 @@
-class Fornecedor:
-    def __init__(self, nome_fornecedor : str, pais_fornecedor : str, termo_pagamento_fornecedor : str) -> None:
-        self.nome_fornecedor = nome_fornecedor
-        self.pais_fornecedor = pais_fornecedor
-        self.termo_pagamento_fornecedor = termo_pagamento_fornecedor
-        pass
