cd2feefae2ed4d12dbf04809546e4635e42e4b86
Lucas <lucassimanf@gmail.com>
2024-05-28 13:58:09 -0300

    Update rascunho.py
    atualização da descrição das classes/métodos e definição de uma classe que ordena os itens cadastrados de acordo com algum parâmetro (nome/categoria) ou filtra, através de um nome.
    
    também foram criadas (porém ainda não implementadas) as classes CompraMaterial e VendaMaterial que serão responsáveis pela compra e venda de materiais, respectivamente. Elas deverão:
    - alterar a quantidade dos materiais
    - imprimir um resumo da transação
    - deve-se verificar posteriormente o impacto delas em outras classes (como Fornecedor e como fazer para integrar as informações do fornecedor etc).

--- rascunho.py
+++ rascunho.py
@@ -10,16 +10,6 @@ class Item:
         self._item_ativo = item_ativo
         self._item_id = 0
 
-
-# -> a ideia aqui será implementar herança para os diferentes tipos de cadastros entre itens vendidos pelo fornnecedor e aqueles em controle no estoque.
-#class ItemEstoque(Item):
-#    def __init__(self, nome : str, quantidade : int, categoria : str, valor : float, item_ativo : bool) -> None:
-#        super().__init__(nome, categoria, item_ativo)
-#    pass
-
-#class ItemFornecedor(Item):
-#    pass
-
 # -> implementa um getter para o nome do item (ex.: se é string etc...)
     @property
     def nome(self) -> str:
@@ -120,7 +110,7 @@ class Fornecedor:
     def termo_pagamento(self, termo_pagamento) -> None:
         self._termo_pagamento = termo_pagamento
 
-    def adicionar_iten_fornecedor(self, item : Type[Item], valor: float)->None:
+    def adicionar_item_fornecedor(self, item : Type[Item], valor: float)->None:
          self._lista_itens.append(item)
          self._valores_itens.append(valor)
 
@@ -141,28 +131,25 @@ class ControleEstoque:
         self._fornecedor_id_controle = 0
         pass
 
-# -> implementa um getter para a lista de itens cadastrados.
+# Getter para a lista de itens cadastrados.
     @property
     def itens_cadastrados(self) -> Type[Item]:
             return self._itens_cadastrados
 
-# -> implementa um getter para a lista de itens cadastrados.
+# Getter para o último ID do item cadastrado no banco de dados.
     @property
     def item_id_controle(self) -> int:
             return self._item_id_controle
     
-# a ideia aqui é implementar uma função que vai verificar se um item
-# está cadastrado em nosso banco de itens cadastrados.
+# Método que implementa uma função que verifica se um item está cadastrado no banco de itens cadastrados.
     def verifica_item_cadastrado(self, item : Type[Item]) -> bool:
         for i in self._itens_cadastrados:
-            if i.nome == item.nome:
+            if i.nome == item.nome and i.nome == item.valor:
                 return True
         else:
              return False 
         
-# a ideia aqui é implementar uma função que vai verificar se um fornecedor
-# está cadastrado em nosso banco de fornecedores cadastrados.
-            
+# Método que verifica se um fornecedor está cadastrado no banco de fornecedores cadastrados.
     def verifica_fornecedor_cadastrado(self, fornecedor : Type[Fornecedor]) -> None:
         for i in self._itens_cadastrados:
             if i.nome == fornecedor.nome_fornecedor:
@@ -170,8 +157,7 @@ class ControleEstoque:
         else:
              return False    
             
-# a ideia aqui é implementar uma função que vai cadastrar um item
-# no nosso banco de itens cadastrados.
+# Método que cadastra um item no banco de itens cadastrados.
     def cadastra_item(self, item : Type[Item]) -> None:
         if (not self.verifica_item_cadastrado(item)):
             self._item_id_controle += 1
@@ -179,8 +165,7 @@ class ControleEstoque:
             self._itens_cadastrados.append(item)
             print(f'{item.nome} {item.item_id} cadastrado com sucesso!')
 
-# a ideia aqui é implementar uma função que vai cadastrar um fornecedor
-# no nosso banco de fornecedores cadastrados.
+# Método que cadastra um fornecedor no banco de fornecedores cadastrados.
     def cadastra_fornecedor(self, fornecedor : Type[Fornecedor]) -> None:
         if (not self.verifica_fornecedor_cadastrado(fornecedor)):
             self._fornecedor_id_controle += 1
@@ -188,31 +173,65 @@ class ControleEstoque:
             self._fornecedores_cadastrados.append(fornecedor)
             print(f'{fornecedor.nome_fornecedor} {fornecedor._id_fornecedor} cadastrado com sucesso!')
 
-# a ideia aqui é implementar uma função que vai remover um item
-# do nosso banco de itens cadastrados.
+
+# "Remove" um item da lista, alterando seu estado para obsoleto.
     def remove_item(self, item : Type[Item]) -> None:
         if self.verifica_item_cadastrado(item):
             item._item_ativo = False
 
-# a ideia aqui é implementar uma função que vai retornar o índice
-# do membro da lista que estamos procurando (e utilizar em outras
-# funções futuramente);
+
+# Retorna o índice do membro da lista.
     def localiza_item(self, item : Type[Item]) -> int:
         for i in self.itens_cadastrados:
             if i.nome == item.nome:
                 return self.itens_cadastrados.index(i)
 
 
-# a ideia aqui é implementar uma classe que vai ser responsável
-# por imprimir o controle
+# Classe responsável por ordenar o que está cadastrado no estoque de acordo com algum parâmetro.
+class OrdenaEstoque:
+
+# Classe responsável por ordenar o que está cadastrado no estoque de acordo com o nome dos itens.
+    def ordena_nome(self, controle_estoque : Type[ControleEstoque]) -> None:
+        controle_estoque_ordenado_nome = sorted(controle_estoque.itens_cadastrados, key=lambda x: x.nome)
+        print(f"{'Nome':<15}\t{'Qtd.':<5}\t{'Categoria':<25}\t{'Valor':<10}")
+        for i in controle_estoque_ordenado_nome:
+            print(f"{i.nome:<15}\t{i.quantidade:<5}\t{i.categoria:<25}\t{i.valor:<10}")
+
+# Classe responsável por ordenar o que está cadastrado no estoque de acordo com a categoria dos itens.
+    def ordena_categoria(self, controle_estoque : Type[ControleEstoque]) -> None:
+        controle_estoque_ordenado_nome = sorted(controle_estoque.itens_cadastrados, key=lambda x: x.categoria)
+        print(f"{'Nome':<15}\t{'Qtd.':<5}\t{'Categoria':<25}\t{'Valor':<10}")
+        for i in controle_estoque_ordenado_nome:
+            print(f"{i.nome:<15}\t{i.quantidade:<5}\t{i.categoria:<25}\t{i.valor:<10}")       
+
+
+# Classe responsável por filtrar o que está cadastrado no estoque de acordo com algum parâmetro.
+class FiltraEstoque:
+
+# método responsável por filtrar o que está cadastrado no estoque de acordo com o nome do item.
+    def filtra_nome(self, controle_estoque : Type[ControleEstoque], nome_filtro : str) -> None:
+        controle_estoque_ordenado_nome = []
+        for i in controle_estoque.itens_cadastrados :
+            if i.nome == nome_filtro:
+                controle_estoque_ordenado_nome.append(i)
+        print(f"{'Nome':<15}\t{'Qtd.':<5}\t{'Categoria':<25}\t{'Valor':<10}")
+        for j in controle_estoque_ordenado_nome:
+            print(f"{j.nome:<15}\t{j.quantidade:<5}\t{j.categoria:<25}\t{j.valor:<10}")       
+            
+
+# Class responsável por imprimir o estado atual do estoque.
 class GerenciadorPlanilha:
+
+# método responsável por imprimir o estado atual no terminal.
     def le_csv(self, nomearquivo):
         with open(nomearquivo, newline='') as csvfile:
             reader = csv.DictReader(csvfile)
             #spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            print(f"{'Nome':<15}\t{'Qtd.':<5}\t{'Categoria':<25}\t{'Valor':<10}")
             for row in reader:
-                print(row['nome'],"\t", row['quantidade'],"\t",row['categoria'],"\t",row['valor'])
-
+                print(f"{row['nome']:15}\t{row['quantidade']:5}\t{row['categoria']:25}\t{row['valor']}")
+                
+# função responsável por imprimir o estado atual do terminal em um arquivo .csv.
     def escreve_csv_colunas_diferentes(self, nomearquivo, controle_estoque : Type[ControleEstoque]):
         with open(nomearquivo, 'w', newline='') as csvfile:
             fieldnames = ['nome', 'quantidade', 'categoria', 'valor']
@@ -221,10 +240,18 @@ class GerenciadorPlanilha:
             for element in controle_estoque.itens_cadastrados:
                 writer.writerow({'nome': element.nome, 'quantidade': element.quantidade, 'categoria': element.categoria, 'valor': element.valor})
 
-       
-def main():
+
+# Classe encarregada de comprar material -> aumenta qtd. de itens.
+class CompraMaterial:
+    pass
 
 
+# Classe encarregada de vender material -> diminui qtd. de itens.
+class VendaMaterial:
+    pass
+
+
+def main():  
     controle_estoque = ControleEstoque()
     gerenciador_planilha = GerenciadorPlanilha()
 
@@ -236,22 +263,32 @@ def main():
 
     item3 = Item("cola", 50, "item de escola", 2.00, True)
     controle_estoque.cadastra_item(item3)
+    
+    item4 = Item("lapis", 10, "item de escola", 2.50, True)
+    controle_estoque.cadastra_item(item4)
+    
     gerenciador_planilha.escreve_csv_colunas_diferentes('teste.csv', controle_estoque)
 
     gerenciador_planilha.le_csv('teste.csv')
 
     fornecedorA = Fornecedor("fornA", "BR", "Faturado")
     controle_estoque.cadastra_fornecedor(fornecedorA)
-    fornecedorA.adicionar_iten_fornecedor(item1,0.35)
-    fornecedorA.adicionar_iten_fornecedor(item2,5.00)
-    fornecedorA.adicionar_iten_fornecedor(item3,7.50)
+    fornecedorA.adicionar_item_fornecedor(item1,0.35)
+    fornecedorA.adicionar_item_fornecedor(item2,5.00)
+    fornecedorA.adicionar_item_fornecedor(item3,7.50)
 
     fornecedorB = Fornecedor("fornB", "US", "Boleto")
     controle_estoque.cadastra_fornecedor(fornecedorB)
-    fornecedorB.adicionar_iten_fornecedor(item1,0.50)
-    fornecedorB.adicionar_iten_fornecedor(item2,4.80)
-    fornecedorB.adicionar_iten_fornecedor(item3,8.60)
+    fornecedorB.adicionar_item_fornecedor(item1,0.50)
+    fornecedorB.adicionar_item_fornecedor(item2,4.80)
+    fornecedorB.adicionar_item_fornecedor(item3,8.60)
 
-    print(controle_estoque.fornecedores_cadastrados[0].printar_item("cola"))
-
+    print(controle_estoque._fornecedores_cadastrados[0].printar_item("cola"))
+    
+    ordena_estoque = OrdenaEstoque()
+    ordena_estoque.ordena_nome(controle_estoque)
+    ordena_estoque.ordena_categoria(controle_estoque)
+    
+    filtra_estoque  = FiltraEstoque()
+    filtra_estoque.filtra_nome(controle_estoque, "lapis")
 main()
