0bf014746691a359a92705a2dc31a54545b8fc64
Lucas <lucassimanf@gmail.com>
2024-06-21 11:54:31 -0300

    Update programador_producao.py

--- programador_producao.py
+++ programador_producao.py
@@ -7,8 +7,10 @@ class ProgramadorProducao:
         for i in controle_estoque.get_itens_cadastrados:
             if i.get_nome_item == nome_item:
                 i.set_item_ativo = True 
+                print(f"Item {i.get_nome_item} foi configurado como ativo!")
     
     def transforma_item_obsoleto(self, controle_estoque : Type[ControleEstoque], nome_item : str) -> None:
         for i in controle_estoque.get_itens_cadastrados:
             if i.get_nome_item == nome_item:
                 i.set_item_ativo = False
+                print(f"Item {i.get_nome_item} foi configurado como obsoleto!")
